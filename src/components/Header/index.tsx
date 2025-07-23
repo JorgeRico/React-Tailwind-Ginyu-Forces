9e9a45b210eca37814d14ff70eef7735e88094a2
JorgeRico <jorge.rico.garriga@gmail.com>
2025-07-23 20:33:40 +0200

    header basic design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 function Header() {
     return (
         <>
-            <div className="bg-black text-white p-3 flex justify-center">
-                <div className="flex justify-start w-5xl bg-black text-white">
-                    <div className="text-center w-32 flex flex-wrap justify-center items-center gap-2">
-                        <img className="h-14 top-1 relative" src="./src/assets/logo.png" alt="Ginyu Forces"></img>
+            <div className="bg-black text-white p-3 flex justify-center fixed top-0 w-full">
+                <div className="flex flex-row w-5xl bg-black text-white flex-start justify-between">
+                    <div className="flex flex-col items-center justify-center gap-2 w-36">
+                        <img className="h-12 top-1 relative" src="./src/assets/logo.png" alt="Ginyu Forces"></img>
                         <span className="font-light text-sm -top-1 relative">The Ginyu Forces</span>
+                    </div>
+                    <div className="gap-1 text-sm w-auto flex items-end justify-center group hover:underline hover:gap-2 px-4">
+                        <img className="relative h-8 bottom-1 group-hover:bottom-2 group-hover:scale-135" src="./src/assets/frieza.png" alt="Ginyu Forces"></img>
+                        <a href="#" className="relative bottom-2 group-hover:bottom-3 group-hover:scale-115 uppercase">hire us</a>
                     </div>
                 </div>
             </div>
