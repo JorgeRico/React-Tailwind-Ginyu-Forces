2addc88b754ca4b37880f15a55c7e2dcc8a4597f
JorgeRico <jorge.rico.garriga@gmail.com>
2025-08-28 11:51:33 +0200

    add animations

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,12 +4,12 @@ function Footer() {
             <div className="bg-black text-white p-10 w-full flex items-center justify-center flex-col">
                 <h2 className="mt-5 mb-15 text-3xl">Sponsors</h2>
                 <div className="flex flex-wrap flex-items justify-center gap-10 mb-15 w-5xl max-md:w-full">
-                    <img src="../assets/sponsors/yamcha.png" alt="Yamcha" className="h-20 mr-4 bg-white p-3" />
-                    <img src="../assets/sponsors/capsulecorp.png" alt="Capsule Corp" className="h-20 mr-4 bg-white p-3" />
-                    <img src="../assets/sponsors/ribbon.png" alt="Red Ribbon" className="h-20 mr-4 bg-white p-3" />
-                    <img src="../assets/sponsors/babidi.png" alt="Babidi" className="h-20 mr-4 bg-white p-3" />
-                    <img src="../assets/sponsors/roshi.png" alt="Mutenroshi" className="h-20 mr-4 bg-white p-3" />
-                    <img src="../assets/sponsors/muten.png" alt="King Mutenroshi" className="h-20 mr-4 bg-white p-3" />
+                    <img src="../assets/sponsors/yamcha.png" alt="Yamcha" className="h-20 mr-4 bg-white p-3 hover:scale-115 transform transition-all" />
+                    <img src="../assets/sponsors/capsulecorp.png" alt="Capsule Corp" className="h-20 mr-4 bg-white p-3 hover:scale-115 transform transition-all" />
+                    <img src="../assets/sponsors/ribbon.png" alt="Red Ribbon" className="h-20 mr-4 bg-white p-3 hover:scale-115 transform transition-all" />
+                    <img src="../assets/sponsors/babidi.png" alt="Babidi" className="h-20 mr-4 bg-white p-3 hover:scale-115 transform transition-all" />
+                    <img src="../assets/sponsors/roshi.png" alt="Mutenroshi" className="h-20 mr-4 bg-white p-3 hover:scale-115 transform transition-all" />
+                    <img src="../assets/sponsors/muten.png" alt="King Mutenroshi" className="h-20 mr-4 bg-white p-3 hover:scale-115 transform transition-all" />
                 </div>
             </div>
             <div className="bg-black/90 text-white px-10 py-3 w-full flex items-center justify-center">
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,19 @@ import { NavLink } from 'react-router-dom';
 function Header() {
     return (
         <>
-            <div className="bg-black text-white p-3 flex justify-center w-full z-10">
-                <div className="flex flex-row w-5xl bg-black text-white flex-start justify-between max-md:w-full">
-                    <NavLink to="/" className="flex flex-col items-center justify-center gap-2 w-36">
-                        <img className="h-16 relative" src="../assets/header/logo.png" alt="Ginyu Forces"></img>
-                    </NavLink>
-                    <div className="gap-1 text-sm w-auto flex items-end justify-center group hover:underline hover:gap-2 px-4">
-                        <img className="relative h-8 bottom-1 group-hover:bottom-2 group-hover:scale-135" src="../assets/header/frieza.png" alt="Ginyu Forces"></img>
-                        <NavLink to="/contact" className="relative bottom-2 group-hover:bottom-3 group-hover:scale-115 uppercase">hire us</NavLink>
+            <header className="w-full fixed top-0 left-0 right-0 z-10">
+                <div className="bg-black text-white p-3 flex justify-center w-full">
+                    <div className="flex flex-row w-5xl bg-black text-white flex-start justify-between max-md:w-full">
+                        <NavLink to="/" className="flex flex-col items-center justify-center gap-2 w-36">
+                            <img className="h-16 relative" src="../assets/header/logo.png" alt="Ginyu Forces"></img>
+                        </NavLink>
+                        <div className="gap-1 text-sm w-auto flex items-end justify-center group hover:underline hover:gap-2 px-4">
+                            <img className="relative h-8 bottom-1 group-hover:bottom-2 group-hover:scale-135" src="../assets/header/frieza.png" alt="Ginyu Forces"></img>
+                            <NavLink to="/contact" className="relative bottom-2 group-hover:bottom-3 group-hover:scale-115 uppercase">hire us</NavLink>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </header>
         </>
     )
 }
